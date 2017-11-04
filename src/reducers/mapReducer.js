c84de90d07fd31ba0742b2f147c428c3d72a05c1
parkerziegler <parkerellisziegler@gmail.com>
2017-11-03 21:15:15 -0700

    Setting up the mapReducer.

--- src/reducers/mapReducer.js
+++ src/reducers/mapReducer.js
@@ -1,6 +1,8 @@
+import * as actions from '../actions/action-types';
+
 const initialState = {
     geoData: [],
-    basemap: "http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg"
+    basemap: "http://c.tile.stamen.com/toner/{z}/{x}/{y}.png"
 };
 
 // "http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg"
@@ -9,6 +11,13 @@ const initialState = {
 const mapReducer = (state = initialState, action) => {
     
     switch(action.type) {
+
+        case actions.SWITCH_BASEMAP:
+        
+            return {
+                ...state,
+                basemap: action.payload.url
+            };
         default:
             return state;
     }
